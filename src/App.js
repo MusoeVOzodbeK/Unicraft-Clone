abb6a749aaad5b8a2a3d411220063540aadd0842
abdusamadweb <abdusamadweb@gmail.com>
2022-09-15 11:06:56 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+// global styles
+import './assets/styles/normalize.css'
+import './assets/styles/flex-box.css'
+import './assets/styles/global.css'
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {/* header */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        {/* footer */}
+      </BrowserRouter>
     </div>
   );
 }
