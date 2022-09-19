d1a5d56b050d94da97c377dca10ec0ce2a5084fa
musoev_ozodbek <musayev@gmail.com>
2022-09-19 06:26:09 +0500

    lang

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,66 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import "../assets/styles/Layout.css";
-import icon from "../photos/image-removebg-preview (3).png"
+import icon from "../photos/image-removebg-preview (3).png";
 function Layout() {
+  const setMenuBar = () => {
+    document.querySelector(".ul-li").classList.toggle("toggle");
+  };
   return (
     <>
       <header className="header">
         <div>
-          <img
-            src={icon}
-            alt=""
-          />
+          <img src={icon} alt="" />
           <p>Unicraft — платформа для онлайн обучения</p>
         </div>
         <ul className="ul-li">
           <li>
-            <Link className="link" to="/">Главная</Link>
+            <Link className="link" to="/">
+              Главная
+            </Link>
           </li>
           <li>
-            <Link className="link" to="/platforma">Платформа</Link>
-              {/* <ul className="ul-hovers">
+            <Link className="link" to="/platforma">
+              Платформа
+            </Link>
+            {/* <ul className="ul-hovers">
                 <li><Link className="link" to="/platforma">Функционал</Link></li>
                 <li>Каталог курсов<i class='bx bx-link-external'></i></li>
               </ul> */}
           </li>
           <li>
-            <Link className="link" to="/tarif">Тарифы</Link>
-              {/* <ul>
+            <Link className="link" to="/tarif">
+              Тарифы
+            </Link>
+            {/* <ul>
                 <li>Стоимость</li>
                 <li>Возможность</li>
                 <li>Частые вапросы</li>
               </ul> */}
           </li>
           <li>
-            <Link className="link" to="/kompaniya">Компания</Link>
+            <Link className="link" to="/kompaniya">
+              Компания
+            </Link>
 
-              {/* <ul>
+            {/* <ul>
                 <li>О нас</li>
                 <li>Отзывы</li>
                 <li>Партнерам</li>
                 <li>Вакансии</li>
                 <li>Контакты</li>
               </ul> */}
-            
           </li>
           <li>
-            <Link className="link" to="/podderjka">Поддержка</Link>
+            <Link className="link" to="/podderjka">
+              Поддержка
+            </Link>
           </li>
           <li>
-            <Link className="link" to="/blog">Блог</Link>
-              {/* <ul>
+            <Link className="link" to="/blog">
+              Блог
+            </Link>
+            {/* <ul>
                 <li>Стать автором</li>
               </ul> */}
           </li>
-		<button>Ru</button>
+          <li>
+            <button className="lang">Ru</button>
+            <ul className="lang-eng">
+              <li><button>Eng</button></li>
+            </ul>
+          </li>
         </ul>
+        <button onClick={setMenuBar}>
+          <i className="bx bx-menu"></i>
+        </button>
       </header>
       <main>
         <Outlet />
       </main>
       {/* footer  */}
-      
     </>
   );
 }
